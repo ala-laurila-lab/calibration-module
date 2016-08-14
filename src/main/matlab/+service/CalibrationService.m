485a3e214c6167f03e45428fbaa9bb073c54cdc6
ragavsathish <sathish_narayanan@outlook.com>
2016-08-15 00:41:20 +0300

    add new measurement and select last measurement

--- src/main/matlab/+service/CalibrationService.m
+++ src/main/matlab/+service/CalibrationService.m
@@ -1,54 +1,33 @@
 classdef CalibrationService < handle
     
-    properties
-        entityManager
+    properties(Access = private)
+        dataManager
+        logManager
     end
     
     methods
         
-        function obj = CalibrationService(rigName)
+        function obj = CalibrationService(data, log)
             
             path = which('symphony-persistence.xml');
-            obj.entityManager = mpa.factory.createEntityManager(rigName, path);
+            obj.dataManager = mpa.factory.createEntityManager(data, path);
+            obj.logManager = mpa.factory.createEntityManager(log, path);
         end
         
-        function add(obj, entity)
-            obj.entityManager.persist(entity);
+        function add(obj, entity, log)
+            entity = obj.dataManager.persist(entity);
+            log.id = entity.id;
+            obj.logManager.persist(log);
         end
-        
-        function e = get(obj, e)
-            em  = obj.entityManager;
+               
+        function measurement = getLastMeasurement(obj, class, key)
             
-            if isempty(e.calibrationDate)
-                import entity.*;
-                query = Measurement.getAllCalibrationDate(e.entityId);
-                dates = em.executeQuery(query);
-                e.calibrationDate = dates(1);
-            end
-            
-            key = e.key;
-            
-            if obj.useCache && isKey(obj.cache, key)
-                e = obj.cache(key);
-                return
-            end
-            
-            em.find(e);
-            
-            if obj.useCache
-                obj.cache(key) = e;
-            end
-        end
-        
-        function map = getCalibrationDates(obj)
-            map = containers.Map();
-            em = obj.entityManager.entityMap;
-            
-            for key = em.keys()
-                query = entity.Measurement.getAllCalibrationDate(em(key{:}));
-                dates = obj.entityManager.executeQuery(query);
-                map(key{:}) = dates;
-            end
+            log = obj.logManager.createQuery('entity.AuditLog')...
+                .where(@(e) strcmp(e.calibrationType, class) && strcmp(e.calibrationKey, key))...
+                .LastOrDefault();
+            constructor = str2func(class);
+            measurement = constructor(log.calibrationId);
+            measurement = obj.dataManager.find(measurement);
         end
         
         function e = getLinearityByStimulsDuration(obj, duration, date, ledType)
