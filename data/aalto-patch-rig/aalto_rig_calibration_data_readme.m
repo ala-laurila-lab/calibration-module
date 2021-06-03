76c6409baa20dddf61d15098a1c08907bd5d4c58
JohanWesto <johan.westo@gmail.com>
2021-06-03 11:29:28 +0300

    Save non linearity json

---
+++ data/aalto-patch-rig/aalto_rig_calibration_data_readme.m
@@ -0,0 +1,10 @@
+% This file is just a documentation script
+% All the patch rig calibartion data will be present in the same folder as the
+% script lives
+
+% The data is present in 
+%  Remote: https://github.com/ala-laurila-lab/calibration-module.git
+%          data/aalto-patch-rig
+
+% Directory
+%  + projector-led-nonlinearity - Contains non linearity measurement for lightcrafter led
---
+++ data/aalto-patch-rig/projector-led-nonlinearity/readme.m
@@ -0,0 +1,21 @@
+%% Script to visualize the non linearity json
+
+% Replace this with actual file name present in the same directory as readme
+NON_LINEARITY_FILE_NAME = 'test.json';
+
+%Load a json file to structure
+location = fileparts(mfilename('fullpath'));
+nonLinearity  = loadjson([location filesep NON_LINEARITY_FILE_NAME]);
+
+% Create ideal linear gamma vector.
+linear = linspace(0, 1, length(nonLinearity.ledCurrent));
+
+h = figure('Name', 'Gamma', 'NumberTitle', 'off');
+a = axes(h);
+plot(a, nonLinearity.ledCurrent, nonLinearity.normalizedMeasurements, '.', nonLinearity.ledCurrent, linear, '-');
+legend(a, 'Measurements', 'Ideal');
+title(a, 'Gamma');
+set(a, ...
+    'FontUnits', get(h, 'DefaultUicontrolFontUnits'), ...
+    'FontName', get(h, 'DefaultUicontrolFontName'), ...
+    'FontSize', get(h, 'DefaultUicontrolFontSize'));
