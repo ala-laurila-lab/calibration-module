6e240f3df867b149a12cef3582d39ec5e5fceec6
Aarni Seppänen <aarni@Aarnis-MacBook-Pro.local>
2021-07-22 16:12:05 +0300

    changed photoreceptor area to photoreceptor collecting area

--- src/main/matlab/+ala_laurila_lab/+util/photonToIsomerisation.m
+++ src/main/matlab/+ala_laurila_lab/+util/photonToIsomerisation.m
@@ -1,4 +1,4 @@
-function isomerization_rate_sum = photonToIsomerisation(power_spectrum, lambda, lambda_max, photoreceptor_area)
+function isomerization_rate_sum = photonToIsomerisation(power_spectrum, lambda, lambda_max, photoreceptor_collecting_area)
 % isomerization_rate calculates photoisomerization rate
 %
 %   power_spectrum
@@ -10,13 +10,13 @@ function isomerization_rate_sum = photonToIsomerisation(power_spectrum, lambda, 
 %   1. Get the photon flux 
 %   2. calculate the absorbance spectrum using GovardovskiiNomogram template
 %   3. calculate isomerization rate which is the product of absorbance 
-%   spectrum , photoreceptor area and number of photons 
+%   spectrum , photoreceptor collecting area and number of photons 
 %   4. return sum of isomerization rate
 %
 import ala_laurila_lab.util.*;
 
 photons = I2Photon(power_spectrum, lambda);
 absorbance_spectrum = myGovardovskiiNomogram(lambda, lambda_max)';
-isomerization_rate = photons .*absorbance_spectrum * photoreceptor_area ;
+isomerization_rate = photons .*absorbance_spectrum * photoreceptor_collecting_area ;
 isomerization_rate_sum = sum(isomerization_rate);
 end
